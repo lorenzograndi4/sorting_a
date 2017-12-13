77e0201adf2a72002a2484bac76ca315ba779dd0
Lorenzo Grandi <lorenzo@wetransfer.com>
2017-12-13 16:34:04 +0100

    Add merge sort

--- sort.rb
+++ sort.rb
@@ -1,8 +1,9 @@
 require 'csv'
 require 'benchmark'
+require File.expand_path('../algorithms/merge', __FILE__)
 
 CSV_FILE = File.expand_path('../datasets/ted_main.csv', __FILE__ )
-COLUMN = 'views'
+COLUMN = 'comments'
 
 data = CSV.read(CSV_FILE, headers: true)
 
@@ -30,14 +31,17 @@ def bubble_sort(array, column)
 end
 
 def insertion_sort(array, column)
+  @calls = 0
   (array.length).times do |j|
     while j > 0
       if array[j-1][column].to_i > array[j][column].to_i
         array[j], array[j-1] = array[j-1], array[j]
+        @calls+=1
       else
         break
       end
       j-=1
+      @calls+=1
     end
   end
   array
@@ -51,4 +55,26 @@ end
 # From 'comments' 3.600000   0.010000   3.610000 (  3.616539)
 # puts Benchmark.measure { insertion_sort(data, COLUMN) }
 
-puts Benchmark.measure { ruby_sort(data, COLUMN) }
+# puts Benchmark.measure { ruby_sort(data, COLUMN) }
+
+array = [
+  { 'views' => '22', 'speakers' => '41', 'name' => 'foo' },
+  { 'views' => '6666', 'speakers' => '2', 'name' => 'bar' },
+  { 'views' => '3333', 'speakers' => '77', 'name' => 'baz' }
+]
+
+array_of_hashes = data.map do |row|
+  row.to_hash
+end.to_a
+
+# For 'views': 0.020000   0.000000   0.020000 (  0.017218)
+# For 'comments': 0.010000   0.000000   0.010000 (  0.012994)
+# puts Benchmark.measure { mergesort(array_of_hashes, COLUMN) }
+
+
+
+
+
+
+
+
